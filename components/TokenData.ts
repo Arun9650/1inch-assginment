d0bc14902dcbfdc56c450f0e430fb50b74b38f69
Arun Kumar <arun02580@gmail.com>
2023-10-15 13:28:00 +0530

    trying to call the api

--- components/TokenData.ts
+++ components/TokenData.ts
@@ -3,7 +3,7 @@ export const TokenData = async ({chain}: {chain:number | undefined}) =>{
     const res = await fetch(`https://api.1inch.dev/token/v1.2/${chain}`, {
         headers: {
           "Content-Type": "application/json",
-          "Authorization":  `Bearer ${process.env.NEXT_PUBLIC_1INCH_TOKEN}` ,
+          "Authorization":  `Bearer wINrkvzrIFDAHM3b9k9DHcR5r13jJoD4` ,
           "accept": "application/json",
         },
       

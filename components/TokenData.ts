713d76720190110e98eaf87a3196c2243356243c
Arun Kumar <arun02580@gmail.com>
2023-10-15 23:04:18 +0530

    fixing the error

--- components/TokenData.ts
+++ components/TokenData.ts
@@ -1,25 +1,59 @@
 import axios from "axios";
 
-export const TokenData = async ({ chain }: { chain: number | undefined }) => {
-  const baseurl = "https://api.1inch.dev/token";
-  const token = "wINrkvzrIFDAHM3b9k9DHcR5r13jJoD4"; // Replace with your API token
+const API_KEY = "JQnOv9gMMhHR3hBlan8WFwOwbIEPd05U"; // Replace with your actual 1inch API key
+const BASE_URL = "https://api.1inch.dev/token";
 
-  // Set the headers with your API token
-  const headers = {
-    Authorization: `Bearer ${token}`,
+const headers = {
+  Authorization: `Bearer wINrkvzrIFDAHM3b9k9DHcR5r13jJoD4`,
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  "Access-Control-Allow-Headers":
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  "Access-Control-Allow-Credentials": "true",
+};
+
+export const get1inchTokenList = async (
+  chainId: number,
+  cfIpCountry?: string | null,
+  provider: string = "1inch",
+  country?: string | null
+) => {
+  const params = {
+    "cf-ipcountry": cfIpCountry,
+    provider,
+    country,
   };
+  const endpoint = `/v1.2/${chainId}/token-list?cf-ipcountry=US&provider=1inch&country=US`;
 
   try {
-    const response = await axios.get(`${baseurl}/v1.2/${chain}`, {
-      headers: headers,
-    });
+    // const response = await axios.get(BASE_URL + endpoint, { headers, params });
 
-    const data = response.data;
-    console.log(data);
-    return data;
+    // if (response.status === 200) {
+    //   return response.data;
+    // } else {
+    //   console.error(`Failed to get 1inch token list. Status code: ${response.status}`);
+    //   return null;
+
+    const res = await fetch(BASE_URL + endpoint, { headers, method: "GET" });
+    const data = await res.json();
   } catch (error) {
-    // Handle any errors that occur during the request
-    console.error("Error:", error);
-    throw error; // Optionally re-throw the error for the calling code to handle
+    console.error("Error: thyakdhfkadj", error);
+    return null;
   }
+};
+
+// Example usage:
+const chainId = 1;
+const cfIpCountry = "US";
+const country = "US";
+
+export const TokenData = async ({ chain }: { chain: number | undefined }) => {
+  get1inchTokenList(chainId, cfIpCountry, "1inch", country).then(
+    (tokenList) => {
+      if (tokenList) {
+        console.log("1inch Token List:", tokenList);
+      }
+    }
+  );
 };
